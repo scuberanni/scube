d273c82b1e0bee0dbdc2d41cc6b4f71c9144692c
SCUBE <148875941+scuberanni@users.noreply.github.com>
2024-03-26 07:45:29 +0530

    new

--- manager/forms.py
+++ manager/forms.py
@@ -1,11 +1,28 @@
 
 from django.forms import ModelForm
 from . models import Scube_ss,orders
+from django import forms
+
 
 class PrForm(ModelForm):
+      pr_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+      sl_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
       class Meta:
             model=Scube_ss
-            fields='__all__'
+            fields=['code','Catogory', 'name', 'size','prize', 'material', 'color','pr_date','sl_date','status','image']
+            labels = {
+            'code': 'Custom Label for Field 1',
+            'Catogory': 'Custom Label for Field 2',
+            'name': 'Custom Label for Field 3',
+            'size': 'Custom Label for Field 4',
+            'prize': 'Custom Label for Field 5',
+            'material': 'Custom Label for Field 6',
+            'color': 'Custom Label for Field 7',
+            'pr_date': 'Custom Label for Field 8',
+            'sl_date': 'Custom Label for Field 9',
+            'status': 'Custom Label for Field 10',
+            'image': 'Custom Label for Field 11',
+        }
 
 class OrderForm(ModelForm):
       class Meta:
